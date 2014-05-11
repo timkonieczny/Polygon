0774a8a85da73e6779f634daedfea5cd47120a1b
timbotombo <timkonieczny@gmail.com>
2014-05-11 15:18:56 +0200

    Enabled immersive mode

--- app/src/main/java/com/timkonieczny/polygon/GameGLSurfaceView.java
+++ app/src/main/java/com/timkonieczny/polygon/GameGLSurfaceView.java
@@ -1,8 +1,13 @@
 package com.timkonieczny.polygon;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.view.MotionEvent;
+import android.view.View;
+
+import org.jetbrains.annotations.NotNull;
 
 public class GameGLSurfaceView extends GLSurfaceView{
 
@@ -18,14 +23,27 @@ public class GameGLSurfaceView extends GLSurfaceView{
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void onWindowFocusChanged(boolean hasFocus){
         super.onWindowFocusChanged(hasFocus);
         mWidth=getWidth();
         mHeight=getHeight();
+
+        if (hasFocus) {
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                this.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent e) {
+    public boolean onTouchEvent(@NotNull MotionEvent e) {
         if (e.getActionMasked()==MotionEvent.ACTION_DOWN){
             GLRenderer.SCREEN_TOUCHED =true;
 
